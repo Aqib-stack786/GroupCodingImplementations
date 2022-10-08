999fca2a2988fd0faa68ede0734000c6507838b4
Aqib-stack786 <69700714+Aqib-stack786@users.noreply.github.com>
2022-10-09 02:45:17 +0500

    Theme Custom Color from BackEnd

---
+++ CustomThemeColor/CustomThemeColor/ActionFilter/CustomActionFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomThemeColor.ActionFilter
+{
+    public class CustomActionFilter : ActionFilterAttribute
+    {
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    //ViewBag.SomeValue = "glorp frob blizz nunk";
+        //    // Access the controller, parameters, querystring, etc. from the filterContext
+        //    base.OnActionExecuting(filterContext);
+        //}
+    }
+
+}
---
+++ CustomThemeColor/CustomThemeColor/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CustomThemeColor.ActionFilter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomThemeColor.Controllers
+{
+    [CustomActionFilter]
+    public class HomeController : Controller
+    {
+        public static string BgColor {
+            get { return "#808080"; } 
+        }
+        public static string BorderColor
+        {
+            get{ return "#757575";}
+        }
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            ViewBag.BgColor = BgColor;
+            ViewBag.BorderColor = BorderColor;
+            // Access the controller, parameters, querystring, etc. from the filterContext
+            base.OnActionExecuting(filterContext);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
